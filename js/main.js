5510cf2f896f748fb09cb15b8440285f91ff118d
sagor ifran <sagorifran.143@gmail.com>
2024-02-29 18:15:27 +0600

    responsive css

--- js/main.js
+++ js/main.js
@@ -37,32 +37,16 @@ $(function () {
 
         responsive: [
             {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
                 breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
-                    dots: false
+                    dots: false,
                 }
             }
         ]
+
     });
+
 
 
     //program slider
